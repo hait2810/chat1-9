3f1152e25c0de289d2bf151845bd4684b096570d
ngvhai.dev@gmail.com <ngvhai.dev@gmail.com>
2023-01-03 14:55:05 +0700

    Update login

--- app.js
+++ app.js
@@ -1,31 +1,44 @@
-var express = require('express');
-var router = require('./routes');
-var app = express()
-var path = require("path")
-var http = require("http").Server(app)
-var io = require("socket.io")(http)
-global.__basedir  =  __dirname;
-global._io  =  io; 
+var express = require("express");
+var router = require("./routes");
+var app = express();
+var path = require("path");
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+global.__basedir = __dirname;
+global._io = io;
 const PORT = process.env.PORT || 80;
 http.listen(PORT, () => {
-    console.log("Nodejs is running", PORT);
-})
+  console.log("Nodejs is running", PORT);
+});
 
-app.use(express.json())
+app.use(express.json());
 
-app.use(router)
-app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname,'src/index.html'))
-} )
-app.get((req,res,next) => {
-        res.io = io
-        next();
-})
+app.use(router);
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "src/index.html"));
+});
+app.get((req, res, next) => {
+  res.io = io;
+  next();
+});
+var users = [];
 _io.on("connection", (socket) => {
+  socket.on("send_user", (data) => {
+    const exitsUser = users.find((item) => item == data);
+    if (exitsUser) {
+      socket.emit("error");
+    } else {
+      users.push(data);
+      socket.username = data;
+      _io.sockets.emit("success", users);
+    }
+  });
+  socket.on("logout", (data) => {
+    users = users.filter((item) => item !== data)
+    _io.sockets.emit("success", users);
+  })
+  _io.sockets.emit("success", users);
+  socket.on("disconnect", () => {
     console.log(socket.id, socket.username);
-    socket.on("disconnect", () => {
-        console.log(socket.id, socket.username);    
-    })
-})
-
-
+  });
+});
--- controllers/index.js
+++ controllers/index.js
@@ -1,13 +1,10 @@
 
 const sendRequest = (req, res, next) => {
-  
   _io.username = req.body.msg
   _io.emit("haine", req.body);
   res.json({ code: 200 });
 };
-const Signup = (req,res,next) => {
-  _io.emit
-}
+
 
 module.exports = sendRequest;
 

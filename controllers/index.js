5d2affe9023d2835b3d7040ce78fa7faa69583ab
ngvhai.dev@gmail.com <ngvhai.dev@gmail.com>
2023-01-04 08:46:35 +0700

    Fix

--- controllers/index.js
+++ controllers/index.js
@@ -6,7 +6,7 @@ const sendRequest = (req, res, next) => {
           return _io.emit("errormess", "em là Hải đấy anh Tiến :v ko bug dc đâu");
         }
         if(req.body.msg == "/clear") {
-          _message = []
+         return _message = []
         }
         _message.push(req.body)
   _io.emit("haine", _message);
